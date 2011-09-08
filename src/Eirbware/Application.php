90946fe725fbd2f6309cbba6d050aa4daea812ee
Grégoire Passault <g.passault@gmail.com>
2011-09-08 17:48:36 +0200

    [Twig] Ajout de nl2br

--- src/Eirbware/Application.php
+++ src/Eirbware/Application.php
@@ -8,8 +8,6 @@ use Silex\Extension\SessionExtension;
 use Silex\Extension\TwigExtension;
 use Silex\Extension\DoctrineExtension;
 
-use Eirbware\Twig\ImageExtension;
-
 /**
  * Classe de base pour les applications web de Eirbware
  *
@@ -91,7 +89,7 @@ class Application extends BaseApplication
         $oldConfigure = isset($app['twig.configure']) ? $app['twig.configure']: function(){};
 	$app['twig.configure'] = $app->protect(function($twig) use ($oldConfigure, $app) {
 	    $oldConfigure($twig);
-	    $twig->addExtension(new ImageExtension);
+	    $twig->addExtension(new Twig\Extension);
 	});
     }
 
